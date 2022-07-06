0d4ad1801bebfd3ec8dc96203419bcd2f86c0b3d
katemayorova <katemayorova0@gmail.com>
2022-07-06 22:10:19 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,31 @@
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    path = "."
+    list_files = os.listdir(path)
+    return HttpResponse(", ".join(list_files))
+
